e449926938e3aba2bced98d1d6b1c4c3e8713809
root <root@DESKTOP-U76LOPF.localdomain>
2026-05-12 21:47:05 +1000

    feat: add route for dashboard and remove restricted width borders in styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,18 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
 import Landing from './Auth/Landing'
+import Dashboard from './Components/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Routes>
         <Route path='/' element={<Landing />}/>
         {/* <Route path='/Login' element={<Login successCallback={authenticate} />}/>
-        <Route path='/Register' element={<Register successCallback={authenticate} />}/>
-        <Route path='/Dashboard' element={<Dashboard token={token} logOut={logOut} onClickHome={() => navigate('/')}/>}/>
-        <Route path='/presentation/:id' element={<EditPresentation token={token} />}/>
+        <Route path='/Register' element={<Register successCallback={authenticate} />}/> */}
+        <Route path='/Dashboard' element={<Dashboard />}/>
+        {/* <Route path='/presentation/:id' element={<EditPresentation token={token} />}/>
         <Route path='/presentation/:id/preview' element={<PreviewPresentation token={token} />}/> */}
       </Routes>
 
