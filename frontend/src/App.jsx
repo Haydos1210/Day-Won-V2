1feb772a5a8b5f1ea272d1a8c06f95dcd2c13051
z5590276 <cflahive@DESKTOP-7RME2CU.localdomain>
2026-05-12 21:49:51 +1000

    feat: implement Register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import Landing from './Auth/Landing'
+import Register from './Auth/Register'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const navigate = useNavigate();
+
+  const authenticate = () => {
+    console.log("User authenticated");
+    navigate('/dashboard');
+  };
 
   return (
     <>
       <Routes>
         <Route path='/' element={<Landing />}/>
-        {/* <Route path='/Login' element={<Login successCallback={authenticate} />}/>
         <Route path='/Register' element={<Register successCallback={authenticate} />}/>
-        <Route path='/Dashboard' element={<Dashboard token={token} logOut={logOut} onClickHome={() => navigate('/')}/>}/>
-        <Route path='/presentation/:id' element={<EditPresentation token={token} />}/>
-        <Route path='/presentation/:id/preview' element={<PreviewPresentation token={token} />}/> */}
+        
+        {/*
+          <Route path='/Login' element={<Login successCallback={authenticate} />}/>
+          <Route path='/Register' element={<Register successCallback={authenticate} />}/>
+          <Route path='/Dashboard' element={<Dashboard token={token} logOut={logOut} onClickHome={() => navigate('/')}/>}/>
+          <Route path='/presentation/:id' element={<EditPresentation token={token} />}/>
+          <Route path='/presentation/:id/preview' element={<PreviewPresentation token={token} />}/>
+        */}
       </Routes>
 
     </>
